34f21957c2c83685e547e2bde70807424ed03e8f
mwas <mwas@DESKTOP-5KLM3E2.Host>
2023-03-05 12:29:03 +0300

    google-clone rebuild

---
+++ components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function Avatar() {
+  return (
+    <img
+      loading='lazy'
+      className="h-11 w-11 rounded-full object-cover p-2  object-top"
+      src='/../mwas.jpg'
+      alt='profile pic'
+    />
+  )
+}
+
+export default Avatar
---
+++ pages/search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import Head from 'next/head'
+function search() {
+  return (
+    <div>
+      <h1>search</h1>
+    </div>
+  )
+}
+
+export default search
