edef5442b20b86363ec7093ab7af6d2c9ed648b2
mwas <mwas@DESKTOP-5KLM3E2.Host>
2023-03-05 19:53:35 +0300

    finished the whole build mocking google APIs for search engine

---
+++ keys.js
@@ -0,0 +1,9 @@
+// API key -  https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyB1rjytqfOcR1IZAar3zbZVoyCoiZAD3j0';
+
+// Context key - https://cse.google.com/cse/create/new
+// <script async src="https://cse.google.com/cse.js?cx=744206567cd40462e">
+// </script>
+{/* <div class="gcse-search"></div> */}
+
+export const CONTEXT_KEY ='744206567cd40462e';
